d909ec5e03be64f37785e71f11ccc07f45256589
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2022-11-11 20:29:18 +0900

    Delete 跡地拡張 directory

--- 跡地拡張/main.js
+++
@@ -1,7 +0,0 @@
-/*COPYRIGHT (c) 2022 BlossomsArchive (C)2022 MizuiMiduki*/
-
-//ノーマルモード
-
-    if($('.ErrorMessage').length){
-            window.location.href = "https://nico.ms/sm38213757?from=0";
-    }
